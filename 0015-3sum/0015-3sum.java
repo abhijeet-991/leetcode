38f57485f6d39f42c5f6096aeeac2acf4d99d9f9
abhijeet-991 <156733890+abhijeet-991@users.noreply.github.com>
2024-08-27 07:20:01 +0530

    Time: 32 ms (46.54%), Space: 51.7 MB (45.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,21 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-    HashSet<List<Integer>> set = new HashSet<>();
-    
-    for (int i = 0; i < nums.length; i++) {
-        HashSet<Integer> hashSet = new HashSet<>();
-        for (int j = i + 1; j < nums.length; j++) {
-            int potentialThirdNum = -(nums[i] + nums[j]);
-            if (hashSet.contains(potentialThirdNum)) {
-                List<Integer> list = Arrays.asList(nums[i], nums[j], potentialThirdNum);
-                Collections.sort(list);
-                set.add(list);
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int firstNumberIndex = 0; firstNumberIndex < nums.length; firstNumberIndex++) {
+            if (firstNumberIndex > 0 && nums[firstNumberIndex] == nums[firstNumberIndex - 1]) {
+                continue;  // Skip duplicates for the first number
             }
-            hashSet.add(nums[j]);
-        }
-    }
-    
-    return new ArrayList<>(set);
-}
+            int secondNumberIndex = firstNumberIndex + 1;
+            int thirdNumberIndex = nums.length - 1;
+            while (secondNumberIndex < thirdNumberIndex) {
+                if (nums[firstNumberIndex] + nums[secondNumberIndex] + nums[thirdNumberIndex] == 0) {
+                    ans.add(Arrays.asList(nums[firstNumberIndex], nums[secondNumberIndex], nums[thirdNumberIndex]));
 
+                    //skip duplicates from the left
+                    while (secondNumberIndex < thirdNumberIndex && nums[secondNumberIndex] == nums[secondNumberIndex + 1]) {
+                        secondNumberIndex++;
+                    }
+                    //skip duplicates from the right
+                    while (secondNumberIndex < thirdNumberIndex && nums[thirdNumberIndex] == nums[thirdNumberIndex - 1]) {
+                        thirdNumberIndex--;
+                    }
+
+                    secondNumberIndex++;
+                    thirdNumberIndex--;
+                } else if (nums[firstNumberIndex] + nums[secondNumberIndex] + nums[thirdNumberIndex] > 0) {
+                    thirdNumberIndex--;
+                } else {
+                    secondNumberIndex++;
+                }
+            }
+        }
+        return ans;
+    }
 }
